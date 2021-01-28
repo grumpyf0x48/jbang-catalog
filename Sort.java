29c9b6ae67c34d5627a70c4b8dcf0203dfffd6de
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2021-01-28 19:11:21 +0100

    Add sort program to illustrate Java stream and Array sort in Java with Jbang and Picocli

---
+++ Sort.java
@@ -0,0 +1,125 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//DEPS info.picocli:picocli:4.5.0
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Spec;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.Callable;
+
+@Command(name = "Sort", mixinStandardHelpOptions = true, version = "Sort 0.1", description = "Sort made with jbang")
+class Sort implements Callable<Integer> {
+
+    @Spec
+    private CommandSpec commandSpec;
+
+    @Option(names = {"-i", "--ignore-case"}, description = "Enable ignore case")
+    private boolean ignoreCase;
+
+    @Option(names = {"-m", "--human-numeric-sort"}, description = "Enable human numeric sort")
+    private boolean humanNumericSort;
+
+    @Option(names = {"-n", "--numeric-sort"}, description = "Enable numeric sort")
+    private boolean numericSort;
+
+    @Option(names = {"-r", "--reverse"}, description = "Enable reverse sort")
+    private boolean reverse;
+
+    @Parameters(description = "Elements to sort")
+    private String[] elements;
+
+    public static void main(final String... args) {
+        int exitCode = new CommandLine(new Sort()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() {
+        if (elements == null) {
+            commandSpec.commandLine().usage(System.out);
+            return 1;
+        }
+        final Comparator comparator = reverse ? Comparator.reverseOrder() : Comparator.naturalOrder();
+        Arrays.stream(elements)
+                .map(element -> ignoreCase ? element.toUpperCase() : element)
+                .map(this::mapElement)
+                .sorted(comparator)
+                .forEach(element -> System.out.printf("%s ", element));
+        System.out.println();
+        return 0;
+    }
+
+    private Object mapElement(final String element) {
+        if (numericSort) {
+            return Long.parseLong(element);
+        }
+        if (humanNumericSort) {
+            return HumanNumeric.parse(element);
+        }
+        return String.valueOf(element);
+    }
+
+    private enum Unit {
+        U(1),
+        K(1000 * U.size),
+        M(1000 * K.size),
+        G(1000 * M.size),
+        T(1000 * G.size);
+
+        private final long size;
+
+        Unit(final long size) {
+            this.size = size;
+        }
+
+        @Override
+        public String toString() {
+            return this == U ? "" : name();
+        }
+    }
+
+    private static class HumanNumeric implements Comparable<HumanNumeric> {
+
+        private final long size;
+        private final Unit unit;
+
+        HumanNumeric(final long size, final Unit unit) {
+            this.size = size;
+            this.unit = unit;
+        }
+
+        public long getHumanSize() {
+            return size * unit.size;
+        }
+
+        public static HumanNumeric parse(final String element) {
+            final char lastChar = element.charAt(element.length() - 1);
+            if (Character.isDigit(lastChar)) {
+                return new HumanNumeric(Long.parseLong(element), Unit.U);
+            }
+            for (final Unit unit : Unit.values()) {
+                if (element.endsWith(unit.name())) {
+                    final String[] strings = element.split(unit.name());
+                    return new HumanNumeric(Long.parseLong(strings[0]), unit);
+                }
+            }
+            throw new IllegalArgumentException("Unknown unit: " + lastChar);
+        }
+
+        @Override
+        public int compareTo(final HumanNumeric human) {
+            return Long.compare(getHumanSize(), human.getHumanSize());
+        }
+
+        @Override
+        public String toString() {
+            return size + unit.toString();
+        }
+    }
+}
