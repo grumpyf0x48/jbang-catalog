957af69411a4f95d8a5786d0948443aa51a35901
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2021-07-02 21:02:39 +0200

    Bump jol dependency Fixed Sonar issues

--- SizeOf.java
+++ SizeOf.java
@@ -1,7 +1,7 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
 //DEPS info.picocli:picocli:4.6.1
-//DEPS org.openjdk.jol:jol-core:0.10
+//DEPS org.openjdk.jol:jol-core:0.16
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -17,22 +17,22 @@ import java.util.concurrent.Callable;
 @Command(name = "SizeOf", mixinStandardHelpOptions = true, version = "SizeOf 0.1", description = "A program to display the memory size of Java primitive types and their corresponding wrapper classes")
 class SizeOf implements Callable<Integer> {
 
-    private static final String[] TYPES = {"byte", "boolean", "char", "short", "int", "float", "long", "double"};
+    private static final String[] DEFAULT_TYPES = {"byte", "boolean", "char", "short", "int", "float", "long", "double"};
 
     @Parameters(index = "0..*", arity = "0..*", description = "The primitive types (default: byte, boolean, char, short, int, float, long, double")
-    private final String[] types = TYPES; // Default value declared here instead with @Parameters because it is an Array
+    private final String[] types = DEFAULT_TYPES; // Default value declared here instead with @Parameters because it is an Array
 
     private static final Map<String, Object> JAVA_TYPES = new HashMap<>();
 
     static {
-        JAVA_TYPES.put("byte", new Byte((byte) 0));
+        JAVA_TYPES.put("byte", Byte.valueOf((byte) 0));
         JAVA_TYPES.put("boolean", Boolean.FALSE);
-        JAVA_TYPES.put("char", new Character('\u0000'));
-        JAVA_TYPES.put("short", new Short((short) 0));
-        JAVA_TYPES.put("int", new Integer(0));
-        JAVA_TYPES.put("float", new Float(0.0f));
-        JAVA_TYPES.put("long", new Long(0L));
-        JAVA_TYPES.put("double", new Double(0.0d));
+        JAVA_TYPES.put("char", Character.valueOf('\u0000'));
+        JAVA_TYPES.put("short", Short.valueOf((short) 0));
+        JAVA_TYPES.put("int", Integer.valueOf(0));
+        JAVA_TYPES.put("float", Float.valueOf(0.0f));
+        JAVA_TYPES.put("long", Long.valueOf(0L));
+        JAVA_TYPES.put("double", Double.valueOf(0.0d));
     }
 
     public static void main(final String... args) {
@@ -44,7 +44,7 @@ class SizeOf implements Callable<Integer> {
     public Integer call() {
         final VirtualMachine virtualMachine = VM.current();
 
-        final boolean defaultTypes = types.equals(TYPES);
+        final boolean defaultTypes = types.equals(DEFAULT_TYPES);
 
         for (int index = 0; index < types.length; index++) {
             final String type = types[index];
