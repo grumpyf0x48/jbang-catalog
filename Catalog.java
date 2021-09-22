824240239424e3c74f5aaa8ad682a9ba92fe495c
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2021-09-22 06:25:08 +0200

    Create Catalog.java to allow edit all catalog files in the same IDEA

---
+++ Catalog.java
@@ -0,0 +1,14 @@
+//FILES README.md
+
+//SOURCES GitClone.java
+//SOURCES GitGet.java
+//SOURCES Html.java
+//SOURCES Links.java
+//SOURCES SizeOf.java
+//SOURCES Sort.java
+//SOURCES Ssh.java
+//SOURCES StatusCode.java
+//SOURCES WhatsNewInJava.java
+
+public class Catalog {
+}
