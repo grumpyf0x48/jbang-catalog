8701423049f9ab287591a9f98d0f49c066bc94b0
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2021-09-29 08:02:54 +0200

    Add ci-build.yml in Catalog.java

--- Catalog.java
+++ Catalog.java
@@ -5,10 +5,14 @@
 //FILES templates/junit4.java.qute
 //FILES templates/junit5.java.qute
 //FILES templates/testng.java.qute
+//FILES .github/workflows/ci-build.yml
 //FILES README.md
 
+import java.io.IOException;
+
 public class Catalog {
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
+        Runtime.getRuntime().exec("ls");
         System.exit(0);
     }
 }
