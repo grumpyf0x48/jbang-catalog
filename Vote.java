70fd655162fb206600aef23fca7e2e02107c1d8f
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2022-03-05 09:38:30 +0100

    Split code insto smaller methods

--- Vote.java
+++ Vote.java
@@ -16,12 +16,14 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.Callable;
 import java.util.stream.Stream;
 
 import static java.nio.file.Files.lines;
 import static java.util.Comparator.reverseOrder;
 import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Stream.iterate;
 
 @Command(name = "Vote", mixinStandardHelpOptions = true, version = "Vote 0.1", description = "Vote")
@@ -49,7 +51,7 @@ public class Vote implements Callable<Integer> {
         final String effectiveOptions = getDistinctLetters(options.toUpperCase());
         final int optionsCount = effectiveOptions.length();
 
-        final Integer[] weightedPoints = iterate(optionsCount, n -> n - 1)
+        final Integer[] weightedPoints = iterate(optionsCount, point -> point - 1)
                 .limit(optionsCount)
                 .toArray(Integer[]::new);
 
@@ -58,27 +60,11 @@ public class Vote implements Callable<Integer> {
         votes.map(String::strip)
                 .filter(vote -> vote.matches("([A-Z] )*[A-Z]"))
                 .map(vote -> vote.toUpperCase().replaceAll("\\s+", ""))
-                .forEach(vote -> {
-                    if (vote.length() > optionsCount) {
-                        throw new IllegalArgumentException("Too many options in: " + vote);
-                    }
-                    if (!vote.equals(getDistinctLetters(vote))) {
-                        throw new IllegalArgumentException("Duplicate option in: " + vote);
-                    }
-                    for (int index = 0; index < optionsCount; index++) {
-                        final Character option = vote.charAt(index);
-                        if (!effectiveOptions.contains("" + option)) {
-                            throw new IllegalArgumentException("Invalid option in: " + vote);
-                        }
-                        final Integer actualPoints = pointsPerOption.getOrDefault(option, 0);
-                        final Integer obtainedPoints = weightedPoints[index];
-                        votePrinter.addVoteOption(option, obtainedPoints, index == optionsCount - 1);
-                        pointsPerOption.put(option, actualPoints + obtainedPoints);
-                    }
-                });
+                .forEach(vote -> addVote(vote, effectiveOptions, optionsCount, weightedPoints, pointsPerOption, votePrinter));
 
         final Map<Character, Integer> sortedPointsPerOption = sortByReverseValue(pointsPerOption);
         votePrinter.addVoteResults(sortedPointsPerOption);
+
         return sortedPointsPerOption;
     }
 
@@ -98,20 +84,44 @@ public class Vote implements Callable<Integer> {
                 .toString();
     }
 
-    private static Map<Character, Integer> sortByReverseValue(final Map<Character, Integer> unSortedMap) {
-        final LinkedHashMap<Character, Integer> sortedMap = new LinkedHashMap<>();
+    private static void addVote(final String vote, final String effectiveOptions, final int optionsCount, final Integer[] weightedPoints, final Map<Character, Integer> pointsPerOption, final VotePrinter votePrinter) {
+        checkVote(vote, optionsCount);
+        for (int optionIndex = 0; optionIndex < optionsCount; optionIndex++) {
+            addVoteOption(vote, effectiveOptions, optionsCount, optionIndex, weightedPoints, pointsPerOption, votePrinter);
+        }
+    }
 
-        unSortedMap.entrySet()
+    private static void checkVote(final String vote, final int optionsCount) {
+        if (vote.length() > optionsCount) {
+            throw new IllegalArgumentException("Too many options in: " + vote);
+        }
+        if (!vote.equals(getDistinctLetters(vote))) {
+            throw new IllegalArgumentException("Duplicate option in: " + vote);
+        }
+    }
+
+    private static void addVoteOption(final String vote, final String effectiveOptions, final int optionsCount, final int optionIndex, final Integer[] weightedPoints, final Map<Character, Integer> pointsPerOption, final VotePrinter votePrinter) {
+        final Character option = vote.charAt(optionIndex);
+        if (!effectiveOptions.contains("" + option)) {
+            throw new IllegalArgumentException("Invalid option in: " + vote);
+        }
+        final Integer actualPoints = pointsPerOption.getOrDefault(option, 0);
+        final Integer obtainedPoints = weightedPoints[optionIndex];
+        final boolean lastOption = optionIndex == optionsCount - 1;
+        votePrinter.addVoteOption(option, lastOption, obtainedPoints);
+        pointsPerOption.put(option, actualPoints + obtainedPoints);
+    }
+
+    private static Map<Character, Integer> sortByReverseValue(final Map<Character, Integer> unsortedMap) {
+        return unsortedMap.entrySet()
                 .stream()
                 .sorted(comparingByValue(reverseOrder()))
-                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
-
-        return sortedMap;
+                .collect(toMap(Entry::getKey, Entry::getValue, (Integer a, Integer b) -> b, LinkedHashMap::new));
     }
 
     private interface VotePrinter {
 
-        default void addVoteOption(final Character option, final Integer obtainedPoints, final boolean lastOption) {
+        default void addVoteOption(final Character option, final boolean lastOption, final Integer obtainedPoints) {
         }
 
         default void addVoteResults(final Map<Character, Integer> sortedPoints) {
@@ -134,7 +144,7 @@ public class Vote implements Callable<Integer> {
         }
 
         @Override
-        public void addVoteOption(final Character option, final Integer obtainedPoints, final boolean lastOption) {
+        public void addVoteOption(final Character option, final boolean lastOption, final Integer obtainedPoints) {
             console.append(option)
                     .append('=')
                     .append(obtainedPoints.toString())
