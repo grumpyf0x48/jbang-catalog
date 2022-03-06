1b1dcc95711330e6c434d8d83a75cc700a489ae8
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2022-03-06 18:19:58 +0100

    Add test suite

---
+++ tests/TestSuite.java
@@ -0,0 +1,10 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//JAVA 17
+//SOURCES VoteTest.java
+
+public class TestSuite {
+    public static void main(final String... args) {
+        VoteTest.main(args);
+    }
+}
